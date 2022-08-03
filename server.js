f6be70019577d508e89819dd377dab3ee7c346e1
Max McLaughlin <maxwelljmack@gmail.com>
2022-08-02 20:21:23 -0700

    changed server.js, added config and schema

--- server.js
+++ server.js
@@ -1,63 +1,68 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+require("dotenv").config();
+const express = require("express");
+// const path = require("path");
+// const fs = require("fs");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sequelize = require("./config/connection");
+// need to change file here
+const routes = require("./controllers/homepageController");
+// need to make this file too
+const helpers = require("./utils/helpers");
+
+const hbs = exphbs.create({
+	helpers,
+});
+
+// need to set session secret on .env
+const sessionSettings = {
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	// maxAge: ,
+	saveUninitialized: false,
+	store: new SequelizeStore({
+		db: sequelize,
+	}),
+};
 const app = express();
-// const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 
-// set up express app to handle dataparsing
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "./public")));
+// Template Engine Setup
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
+// middlewares
+app.use(express.static("public"));
+app.use(session(sessionSettings));
+app.use(express.json());
+// app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 // serves index page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-});
-
-// serves home page
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/home.html'))
-});
-
-
-
-// creates the bridge to api and serves the db.json when api/notes is entered in url
-app.get('/api/data', (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
-});
-
-// sends note list data the notes.html page with the provided note list data and serves the db.json
-// app.post('/api/data', (req, res) => {
-//     let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-//     let note = req.body;
-//     savedNotes[note.id] = note;
-//     fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
+// app.get("/", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "./public/index.html"));
 // });
 
-// deletes the selected note, searching via ID
-app.delete("/api/data/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-    let noteID = req.params.id;
-    console.log(typeof noteID);
-    console.log('deleting note# ' + noteID);
-    let newID = 0;
-    // Deleting note based on ID
-    savedNotes.splice(noteID, 1);
-  
-    // resetting note IDs by order
-    for (note of savedNotes) {
-      note.id = newID.toString();
-      newID++;
-    }
-    
-    fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
-  })
+// serves home page
+// app.get("/home", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "./public/home.html"));
+// });
 
-  // Catches all to send to home page
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+// // creates the bridge to api and serves the db.json when api/notes is entered in url
+// app.get("/api/data", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "/db/db.json"));
+// });
+
+// // Catches all to send to home page
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
+
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log("WE MADE IT!!! "));
 });
-
-app.listen(PORT, () => console.log(`Server is listening to PORT ${PORT}`));
